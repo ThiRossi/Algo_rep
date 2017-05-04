3f53667bb294f41175bcc95dce883517639e9b0e
ThiRossi <rosssignol.thibault@gmail.com>
2017-05-04 14:57:43 +0200

    Polygone sa mère

--- algo/src/Polygone.java
+++ algo/src/Polygone.java
@@ -1,4 +1,21 @@
+import java.util.ArrayList;
+
 
 public class Polygone {
+	private ArrayList<Sommet> listSom;
+	
+	public Polygone(ArrayList<Sommet> listSommets){
+		listSommets=listSom;
+	}
+	public void setListSom(ArrayList<Sommet> listSom) {
+		this.listSom = listSom;
+	}
+	public ArrayList<Sommet> getListSom() {
+		return listSom;
+	}
 
+	public String toString() {
+		return "Polygone [listSom=" + listSom + "]";
+	}
+	
 }
